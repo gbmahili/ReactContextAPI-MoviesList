e85a4eebc1d3c583d51f24578521d1be4a54bb02
Baraka Mahili <georgesmahili@gmail.com>
2019-09-02 10:19:06 -0400

    Added Movie.js component

---
+++ src/MovieList.js
@@ -0,0 +1,33 @@
+import React, {useState} from 'react';
+import Movie from './Movie';
+
+const MovieList = () => {
+   const [movies, setMovies] = useState([
+      {
+         name:'Harry Poter',
+         price:'$10',
+         id:12341
+      },
+      {
+         name:'The Hobbits',
+         price:'$19',
+         id:12341
+      },
+      {
+         name:'Lord of The Rings',
+         price:'$21',
+         id:12341
+      },
+   ]);
+   return (
+      <div>
+      {
+         movies.map(movie => (
+            <Movie name={movie.name}/>
+         ))
+      }
+      </div>
+   );
+}
+
+export default MovieList;
